481359304643227b5b8daf446530c87d4af5e45e
luzhaopan <745003249@qq.com>
2022-07-26 15:08:40 +0800

    feat: 配置 html-webpack-plugin ，打包自动生成html文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js', // 配置入口
@@ -9,5 +10,13 @@ module.exports = {
   },
 
   mode: 'none',
+
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './index.html', // 以当前根目录下的index.html为模板
+        filename: 'app.html', // 打包生成的文件名称
+        inject: 'body' // 在body当中引入script
+    }) // 这将会生成一个包含以下内容的 dist/index.html 文件
+  ], 
 
 };
